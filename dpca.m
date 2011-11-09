bd5bcfde630f491913fd9abd8c90a942d65f2ddb
Lailson Bandeira <lailson@guava.com.br>
2011-11-09 10:12:42 -0300

    Finished DPA basic algorithm.

--- dpca.m
+++ dpca.m
@@ -1,50 +1,58 @@
 % Original DPCA algorithm
-function [PC,V] = dpca(X, X_labeled, labels, eta, lambda, d)
-
-equals = @(x,y) x == y;
-[omegaBi omegaBj] = makeSet(X_labeled, labels, equals);
-
-notEquals = @(x,y) x ~= y;
-[omegaWi omegaWj] = makeSet(X_labeled, labels,  notEquals);
-
-Sb = scatterMatrix(omegaBi, omegaBj);
-Sw = scatterMatrix(omegaWi, omegaWj);
-
-St = cov(X);
-
-S = Sb - eta*Sw + lambda*St;
-
-%calcular autovetores e ordenar...
-end
-
-
-function M = scatterMatrix(seti, setj)
-
-xi = seti;
-xj = setj;
-
-n = size(seti, 1);
-S = zeros(size(seti, 2));
-for k = 1:n
-    S = S + (xi(k, :) - xj(k, :))' * (xi(k, :) - xj(k, :));
-end
-M = S/n;
-end
-
-function [Si Sj] = makeSet(dataset, labels, compare)
-    rows = size(dataset,1);
-    cols = size(dataset,2);
-    Si = [];
-    Sj = [];
-    
-    k = 1;
-    for i = 1:rows
-        for j = 1:rows
-           if(i ~= j && compare(labels(i), labels(j)))
-               Si = cat(1, Si, dataset(i,:)); 
-               Sj = cat(1, Sj, dataset(j,:)); 
-               k = k + 1;
-           end
-        end
+function [PC,V] = dpca(X, X_labeled, labels, eta, lambda)
+  % X          matrix with all examples (each column is an example)
+  % X_labeled  matrix with labeled examples (each column is an example)
+  % labels     row-vector with labels of X_labels matrix
+  
+  n_labeled = size(X_labeled, 2);
+  if(n_labeled ~= size(labels,2))
+    error('Number of labels do not match number of labeled examples');
+  end
+  
+  % Omega sets
+  O_b = [];
+  O_w = [];
+  combinations = nchoosek(1:n_labeled,2);
+  for k = 1:size(combinations,1)
+    i = combinations(k,1);
+    j = combinations(k,2);
+    if i ~= j
+      if labels(i) == labels(j)
+        O_w(end+1,:) = combinations(k,:);
+      else
+        O_b(end+1,:) = combinations(k,:);
+      end
     end
+  end
+  
+  % S_b
+  S_b = 0;
+  for k = 1:size(O_b,1)
+    i = O_b(k,1);
+    j = O_b(k,2);
+    x_i = X_labeled(:,i);
+    x_j = X_labeled(:,j);
+    S_b = S_b + (x_i - x_j)*(x_i - x_j)';
+  end
+  S_b = S_b/size(O_b,1);
+  
+  % S_w
+  S_w = 0;
+  for k = 1:size(O_w,1)
+    i = O_w(k,1);
+    j = O_w(k,2);
+    x_i = X_labeled(:,i);
+    x_j = X_labeled(:,j);
+    S_w = S_w + (x_i - x_j)*(x_i - x_j)';
+  end
+  S_w = S_w/size(O_w,1);
+  
+  % S_t
+  S_t = cov(X',1);
+  
+  [PC, V] = eig(S_b - eta*S_w + lambda*S_t);
+  V = diag(V);
+  [~,rindices] = sort(-1*V);
+  V = V(rindices);
+  PC = PC(:,rindices);
 end
